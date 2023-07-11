cd1deb694ad83e1b1c4af6735197526c26c78c25
Karthik1224 <durgamkarthik150@gmail.com>
2023-07-11 21:12:00 +0530

    changes updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
-        obj.setter("Encapsulation");
-        System.out.println(obj.getters());
+        obj.setName("Encapsulation");
+        System.out.println(obj.getName());
     }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,11 +2,11 @@ package com.driver;
 
 public class RWOnly {
     private String name;
-    public void setter(String name)
+    public void setName(String name)
     {
         this.name = name;
     }
-    public String getters()
+    public String getName()
     {
         return name;
     }
